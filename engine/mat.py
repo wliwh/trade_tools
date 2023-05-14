0677725df9ec0f056bb52642c7ba3c5698174dc4
wliwh <wanghn0409@gmail.com>
2023-05-14 23:58:54 +0800

    data update

---
+++ engine/mat.py
@@ -0,0 +1,11 @@
+import akshare as ak
+
+funds_dic = {
+    '纯债债基': '930609',
+    '主动股基': '930890',
+    '普通混基': '930892',
+    '灵活混基': '930893',
+    '偏股基金': '930850'
+}
+
+# ak.stock_zh_index_hist_csindex
